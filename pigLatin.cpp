6a1d50168faf035989a1c31ef242be328b8e632b
Daria Bernice <daria.bernice.calitis@mths.ca>
2021-10-27 21:50:46 +0000

    updated pigLatin.cpp

--- pigLatin.cpp
+++ pigLatin.cpp
@@ -2,13 +2,13 @@
 //
 // Created by: Daria Bernice Calitis
 // Created on: Oct 2021
-// This program determines the smallest number
+// This program converts the sentence to Pig Latin
 
 #include <iostream>
 #include <string>
 #include <cstring>
 #include <vector>
-
+#include <algorithm>
 
 std::string pigLatin(std::string sentence) {
     // This function converts normal text to Pig Latin
@@ -20,8 +20,12 @@ std::string pigLatin(std::string sentence) {
     std::string newWord;
     std::string newSentence;
 
+    /* https://www.programiz.com/c-programming/
+    library-function/string.h/strcpy */
     strcpy(sentenceChars, sentence.c_str());
 
+    /* https://www.tutorialspoint.com/c_standard_library
+    /c_function_strtok.htm */
     // get the first token
     char *token = strtok(sentenceChars, " ");
 
@@ -32,16 +36,23 @@ std::string pigLatin(std::string sentence) {
         token = strtok(NULL,  " ");
     }
 
-    for (int counter = 0; counter < words.size(); counter++) {
-        newWord = (words[counter].erase(0, 1) + words[counter].substr(0, 1)
+    // conversion
+    for (std::string word : words) {
+        newWord = (word.erase(0, 1) + word.substr(0, 1)
         + "ay");
         newWords.push_back(newWord);
     }
 
+    // joining strings
     newSentence = "";
     for (std::string word : newWords) {
         newSentence += word + " ";
     }
+
+    /* https://stackoverflow.com/questions/313970/
+    how-to-convert-an-instance-of-stdstring-to-lower-case */
+    std::transform(newSentence.begin(), newSentence.end(), newSentence.begin(),
+    [](unsigned char c){ return std::tolower(c); });
 
     return newSentence;
 }
@@ -57,6 +68,7 @@ int main() {
     std::cout << "Enter a sentence: ";
     getline(std::cin, user_input);
 
+    // process & output
     std::cout << "\n" << pigLatin(user_input) << std::endl;
 
     std::cout << "\nDone." << std::endl;
